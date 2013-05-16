a1c6341182a4528eb38f0e8ba2c4cc660b181cf9
tsux89 <tsux89@yahoo.com>
2013-05-16 15:00:57 +0900

    Create SepakTakraw.rb
    5 / 16 Thu. Global Education 1.
    Topic : Sepak Takraw

---
+++ SepakTakraw.rb
@@ -0,0 +1,4 @@
+require 'pp'
+
+
+put("Sepak takraw or kick volleyball, is a sport native to the Malay-Thai Peninsula.\nSepak takraw differs from the similar sport of volleyball in its use of a rattan ball and only allowing players to use their feet, knee, chest and head to touch the ball.\nIt is a popular sport in Southeast Asia.\nBut It is not popular in Japan.\n"SEPAK" means "kick" in Malay.\nAnd "TAKRAW" means "ball" in Thai.\nA Japan team composed of university students debuted—along with the sports itself—at the 1990 Asian Games in Beijing.\nWhile there are no professional teams in Japan, colleges such as Asia University, Chiba University, Waseda University and Keio University have formed their own teams.\n")
